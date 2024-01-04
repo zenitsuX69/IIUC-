11974de1bf80f222cf4b1215164791930bec76fe
zenitsuX69 <152077044+zenitsuX69@users.noreply.github.com>
2024-01-04 22:45:30 +0600

    Add files via upload

---
+++ TEST.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Input first number
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    // Input second number
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Calculate sum
+    sum = num1 + num2;
+
+    // Display the sum
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
